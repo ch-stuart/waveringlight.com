fb1c4c7f1f34e81648f4420c097782ea99246b0b
Charles Stuart <cs@enure.net>
2026-05-11 09:57:17 -0600

    feat: enable prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static",
   outDir: "./docs",
+  prefetch: true,
   site: "https://www.waveringlight.com",
   vite: {
     build: { emptyOutDir: false },    
